511cd6d2bafb775c589c8ee9085f01e73f8f6859
FirstBright <mipo8890@gmail.com>
2024-06-14 17:44:03 +0900

    aasdfafsd

--- plugins/shell/select_fruit.sh
+++ plugins/shell/select_fruit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 FRUIT=$1
 echo $1
-if [ $FRUIT == APPLE ];then
+if [ $FRUIT==APPLE ];then
 	echo "You selected Apple!"
-elif [ $FRUIT == ORANGE ];then
+elif [ $FRUIT==ORANGE ];then
 	echo "You selected Orange!"
-elif [ $FRUIT == GRAPE ];then
+elif [ $FRUIT==GRAPE ];then
 	echo "You selected Grape!"
 else
 	echo "You selected other Fruit!"
