7a89fe79f212e4ebbb6e74357a22b5639c846f2e
FirstBright <mipo8890@gmail.com>
2024-06-17 15:04:29 +0900

    add macro dag2

---
+++ dags/dags_bash_with_macro_eg2.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id ='dags_bash_with_macro_eg2',
+    schedule = '10 0 * * 6#2',
+    start_date= pendulum.datetime(2024,5,1, tz="Asia/Seoul"),
+    catchup= False
+) as dag:
+    bash_task_2 = BashOperator(
+        task_id = 'bash_task_2',
+        env={'START_DATE':'{{(data_interval_end.in_timezone("Asia/Seoul"))-macro.dateutil.relativedelta.relativedelta(days=19)|ds}}',
+            'END_DATE':'{{(data_interval_end.in_timezone("Asia/Seoul"))-macro.dateutil.relativedelta.relativedelta(days=14)|ds}}'}
+    )
