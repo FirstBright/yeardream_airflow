90e11d668e30f4dddd5c3efb5a7dfbc3aa064ae3
FirstBright <mipo8890@gmail.com>
2024-06-17 13:39:13 +0900

    fix deco param

--- dags/dags_python_decorator_with_param.py.py
+++ dags/dags_python_decorator_with_param.py.py
@@ -18,9 +18,8 @@ with DAG(
         phone = kwargs['phone'] or 'empty'
         print(f'email: {email}')
         print(f'phone: {phone}')
-        from pprint import pprint
-        pprint(kwargs.get(data_interval_start))
-        pprint(kwargs.get(data_interval_end))
+        print(kwargs.get(data_interval_start))
+        print(kwargs.get(data_interval_end))
 
 
     python_task_1 = regist3('hjkim', 'man', 'seoul', email='hjkim_sun@naver.com', phone='010')
