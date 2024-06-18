b7b11bdc51c85dd56674b91452f14fc6da6dc2f1
FirstBright <mipo8890@gmail.com>
2024-06-18 22:32:57 +0900

    add custom sensor and sensor dag

---
+++ dags/dags_custom_sensor2.py
@@ -0,0 +1,15 @@
+from sensors.seoul_api_date_column_sensor import SeoulApiDateColumnSensor
+from airflow import DAG
+import pendulum
+
+with DAG(
+    dag_id='dags_custom_sensor',
+    start_date=pendulum.datetime(2024,6,14, tz='Asia/Seoul'),
+    schedule='0 9 * * *',
+    catchup=False
+) as dag:
+    sensor__tb_cycle_rent_use_day_info = SeoulApiDateColumnSensor(
+        task_id='sensor__tb_cycle_rent_use_day_info',
+        dataset_nm='ListAirQualityByDistrictService',
+        check_date='{{data_interval_start.in_timezone("Asia/Seoul") | ds_nodash }}'
+    )
---
+++ plugins/sensors/seoul_api_date_column_sensor.py
@@ -0,0 +1,41 @@
+from airflow.sensor.base import BaseSensorOperator
+from airflow.hooks.base import BaseHook
+
+class SeoulApiDateColumnSensor(BaseSensorOperator):
+    template_fields = ('endpoint')
+
+    def __init__(self,dataset_nm,check_date,**kwargs):
+        super().__init__(**kwargs)
+        self.http_conn_id = 'openapi.seoul.go.kr'
+        self.endpoint = '{{var.value.apikey_openapi_seoul_go_kr}}/json/' + dataset_nm +'/1/5'
+        self.check_date = check_date
+
+    def poke(self,context):
+        import requests
+        import json
+        connection = BaseHook.get_connection(self.http_conn_id)
+        url = f'http://{connection.hos}:{connection.port}/{self.endpoint}/'
+        self.log.info(f'url:{url}')
+        response = requests.get(url)
+        contents = json.loads(response.text)
+        self.log.info(f"response: {contents}")
+        if self.find_check_date_in_json(contents, self.check_date):
+            self.log.info('데이터 갱신 확인')
+            return True
+        else:
+            self.log.info('데이터 미갱신')
+            return False
+
+    def find_check_date_in_json(data, check_date):
+        if isinstance(data, dict):
+            for key, value in data.items():
+                if find_check_date_in_json(value, check_date):
+                    return True
+        elif isinstance(data, list):
+            for item in data:
+                if find_check_date_in_json(item, check_date):
+                    return True
+        elif isinstance(data, str):
+            if check_date in data:
+                return True
+        return False
